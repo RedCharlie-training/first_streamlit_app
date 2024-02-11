e3a02aad282bec5866b8eda8d3147f912bd90f76
RedCharlie-training <159731471+RedCharlie-training@users.noreply.github.com>
2024-02-11 16:41:31 -0600

    Update streamlit_app.py
    Adding yet more menu options

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,3 +3,6 @@ import streamlit
 streamlit.title('My Parents New Healthy Diner')
 streamlit.header('Breakfast Menu')
 streamlit.text('Gluten Free King Cake')
+streamlit.text('Omega 3 & Blueberry Oatmeal')
+streamlit.text('Kale, Spinach & Arugula Smoothie')
+streamlit.text('Hard-Boiled Free-Range Egg')
