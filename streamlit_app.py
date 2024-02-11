843c0b8a8a766f643bad4b7126c7956afe1e2971
RedCharlie-training <159731471+RedCharlie-training@users.noreply.github.com>
2024-02-11 16:44:55 -0600

    Update streamlit_app.py
    Adding Avocado Toast

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,8 +1,9 @@
 import streamlit
 
-streamlit.title('My Parents New Healthy Diner')
+streamlit.title('Charlie's New Healthy Diner')
 streamlit.header('Breakfast Menu')
 streamlit.text('Gluten Free King Cake')
 streamlit.text('Omega 3 & Blueberry Oatmeal')
 streamlit.text('Kale, Spinach & Arugula Smoothie')
 streamlit.text('Hard-Boiled Free-Range Egg')
+streamlit.text('We boast of our Avocado Toast')
