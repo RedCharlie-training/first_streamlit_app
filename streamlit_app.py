5d063962257572789bccd1cc00abd0eddaced592
RedCharlie-training <159731471+RedCharlie-training@users.noreply.github.com>
2024-02-13 17:40:46 -0600

    Update streamlit_app.py
    answered questions in comments

--- streamlit_app.py
+++ streamlit_app.py
@@ -26,7 +26,7 @@ streamlit.header('Fruityvice Fruit Advice!')
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
 streamlit.text(fruityvice_response.json())
 
-# write your own comment -what does the next line do? 
+# write your own comment -what does the next line do? Normalizes the data, i.e. like a flat file
 fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
-# write your own comment - what does this do?
+# write your own comment - what does this do? it displays the normalized data
 streamlit.dataframe(fruityvice_normalized)
