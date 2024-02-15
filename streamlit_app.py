9ed72672fbbc555b11a5925c71da770f881a527b
RedCharlie-training <159731471+RedCharlie-training@users.noreply.github.com>
2024-02-14 20:22:03 -0600

    Update streamlit_app.py
    try except

--- streamlit_app.py
+++ streamlit_app.py
@@ -25,15 +25,26 @@ fruits_to_show = my_fruit_list.loc[fruits_selected]
 streamlit.dataframe(fruits_to_show)
 
 streamlit.header('Fruityvice Fruit Advice!')
-fruit_choice = streamlit.text_input('What fruit would you like information about?','Kiwi')
-streamlit.write('The user entered ', fruit_choice)
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
+try:
+   fruit_choice = streamlit.text_input('What fruit would you like information about?','Kiwi')
+   if not fruit_choice:
+      streamlit.error("Please enter a fruit to get information about.")
+   else
+      fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
+      fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
+      streamlit.dataframe(fruityvice_normalized)
+
+  execpt URLError as e:
+    streamlit.error()
+
+#streamlit.write('The user entered ', fruit_choice)
+# fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
 # streamlit.text(fruityvice_response.json())
 
 # write your own comment -what does the next line do? Normalizes the data, i.e. like a flat file
-fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
+
 # write your own comment - what does this do? it displays the normalized data
-streamlit.dataframe(fruityvice_normalized)
+
 
 my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
 my_cur = my_cnx.cursor()
