67757192778d5af0750efc0a20b84b891a3005d0
RedCharlie-training <159731471+RedCharlie-training@users.noreply.github.com>
2024-02-14 20:28:14 -0600

    Update streamlit_app.py
    fix unindent error

--- streamlit_app.py
+++ streamlit_app.py
@@ -34,8 +34,8 @@ try:
       fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
       streamlit.dataframe(fruityvice_normalized)
 
-  execpt URLError as e:
-    streamlit.error()
+execpt URLError as e:
+   streamlit.error()
 
 #streamlit.write('The user entered ', fruit_choice)
 # fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
